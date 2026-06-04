ec64c6b13766609bf798a07452aadd2ddf2593e3
Gaurav Kumar Sinha <gaurav@substrai.dev>
2026-06-04 08:24:01 -0500

    chore: bump version to 0.9.0 for PyPI release
    Signed-off-by: Gaurav Kumar Sinha <gaurav@substrai.dev>

--- src/guardrailgraph/__init__.py
+++ src/guardrailgraph/__init__.py
@@ -8,7 +8,7 @@ from guardrailgraph.core.context import CheckContext
 from guardrailgraph.core.ab_testing import ABTest, ab_test
 from guardrailgraph.core.human_review import ReviewQueue, ReviewRequest, ReviewStatus
 
-__version__ = "0.8.0"
+__version__ = "0.9.0"
 __all__ = [
     "Action",
     "check",
